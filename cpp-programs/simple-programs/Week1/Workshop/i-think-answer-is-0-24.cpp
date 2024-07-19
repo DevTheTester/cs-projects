0107bd8cd25706b59035abe82186986069399833
DevTheTester <dev.kahika.campbell@gmail.com>
2024-07-19 17:09:38 +1200

    prep for practice

---
+++ cpp-programs/simple-programs/Week1/Workshop/i-think-answer-is-0-24.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int num1, num2, product;
+int main() {
+ num1 = 3;
+ num2 = 8;
+ product = num1 * num2;
+ cout << product % 6 << endl;
+ cout << product << endl;
+}
