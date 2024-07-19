de85c57cbb08b5ab9cbe2e5d711a2186af40fc94
DevTheTester <dev.kahika.campbell@gmail.com>
2024-07-19 16:07:31 +1200

    add input 3 numbers

---
+++ cpp-programs/simple-programs/Week1/ThreeNumbers/three-num.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+    int a, b, c;
+
+int main(void){
+    cout << "Type in three numbers: ";
+    cin >> a >> b >> c;
+    cout << "The numbers are " << a << b << c;
+    return 0;
+}
