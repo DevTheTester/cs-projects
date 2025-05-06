ac9a8182d4997229e108b9859bc24960f6f86099
DevTheTester <dev.kahika.campbell@gmail.com>
2025-05-07 06:23:56 +1200

    Add all store/load/add operations

--- cpp-programs/y2-programs/assignment2.cpp
+++ cpp-programs/y2-programs/assignment2.cpp
@@ -55,6 +55,7 @@ int main(){
     pc = 0;
     //Start executing program
     while (true) {
+        cout << endl;
         // Get instruction using program counter
         ir = memory[pc];
         // Get operation code from instruction
@@ -65,56 +66,50 @@ int main(){
         fourth_digit = ir & 0xF;
         xy = ir & 0xFF;
 
-        // 1RXY Load register R with the value XY.
-        // e.g. 1213 means load register R2 with the hexadecimal number 13 (19 in decimal).
-        if (op_code == 1){
-            reg[second_digit] = xy;
-        }
+        // Print instruction
+        cout << "Instruction = " << memory[pc] << endl;
 
-        
-        printRegisterValues();
+        switch (op_code){
+            case 1:
+                reg[second_digit] = xy;
+                cout << "Operation: 1RXY Load register R with the value XY\n" 
+                << "Loading reg[" << second_digit << "] with " << xy << endl;
+                break;
+           case 2:
+                reg[third_digit] = reg[fourth_digit];
+                cout << "Operation: 20RS Load register R with the number in register S\n"
+                << "Loading reg[" << third_digit << "] with " << reg[fourth_digit] << endl;
+                break;
+           case 3:
+                reg[second_digit] = memory[xy];
+                cout << "Operation: 3RXY Load register R with the number in the memory location at address XY\n"
+                << "Loading reg[" << second_digit << "] with " << memory[xy] << endl;
+                break;
+            case 4: 
+                memory[xy] = reg[second_digit];
+                cout << "Operation: 4RXY Store the number in register R in the memory location at address XY\n"
+                << "Loading memory[" << xy << "] with " << reg[second_digit] << endl;
+                break;
+            case 5: 
+                cout << "Operation 5 is not supported\n";
+                break;
+            case 6:
+                reg[second_digit] = reg[third_digit] + reg[fourth_digit];
+                cout << "Operation: 6RST Add the numbers in registers S and T and load the result into register R\n"
+                << "Loading reg[" << second_digit << "] with " << reg[third_digit] << " + " << reg[fourth_digit] << endl;
+                break;
+        }
         //Increase program counter / loop to next instruction
         pc++;
-        break;
+        if (pc > 6){
+            cout << endl;
+            printRegisterValues();
+            break;
+        }
     }
-    
-    
-    
-    // Save register R as a pointer to a memory location
-
-
-    // 20RS Load register R with the number in register S.
-    // e.g. 20A7 means load register R10 with the number in register R7.
-    // 3RXY Load register R with the number in the memory location at address XY.
-    // e.g. 34AB means load register R4 with the contents from memory location AB
-    // 4RXY Store the number in register R in the memory location at address XY.
-    // e.g. 45B1 means store the contents of register R5 in the memory location B1
-    // 5RST Add the numbers in registers S and T and load the result into register R.
-    // Note: this is floating point addition.
-    // e.g. 534E means add the numbers in R4 and R14 and load the result into R3.
-    // 6RST Add the numbers in registers S and T and load the result into register R.
-    // Note: this is integer addition using 2’s complement arithmetic.
-    // e.g. 6823 means add the contents of R2 and R3 and load the result into R8.
-    // 70R0 Negate register R. (Carry out complement and add 1 – in effect, multiply R with -1)
-    // e.g. 70A0 means multiply the number in R10 by -1.
-    // 8R0X Shift the number in register R to the right X times.
-    // e.g. 8403 means that the number in R4 must be shifted 3 bits to the right.
-    // 9R0X Shift the number in register R to the left X times.
-    // e.g. 9602 means that the number in R6 must be shifted 2 bits to the left.
-    // ARST AND the numbers in registers S and T and load the result into register R.
-    // e.g. A045 means AND the numbers in R4 and R5 and load the result into R0.
-    // BRST OR the numbers in registers S and T and load the result into register R.
-    // e.g. BC74 means OR the numbers in R7 and R4 and load the result into R12.
-    // CRST XOR the numbers in registers S and T and load the result into register R.
-    // e.g. C5F3 means XOR the numbers in R15 and R3 and load the result into R5.
-    // DRXY Jump to the instruction at address XY if the value in register R is equal
-    // to the value in register R0.
-    // e.g. D43C means the following: (a) compare the contents of R4 with R0.
-    // (b) if they are equal load 3C into the program counter.
-    // E000 Halt
 }
 
- int printRegisterValues(){
+int printRegisterValues(){
     for (int i = 0; i < 16; i++){
         cout << "reg[" << i << "] = " << hex << reg[i] << endl;
     }
